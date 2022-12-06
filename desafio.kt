942cb79fc9e91a493a6f32e89f32e33aa27bd7c8
Lohan <111451137+lohan-ribeiro@users.noreply.github.com>
2022-12-06 03:24:30 -0300

    Update-desafio

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,45 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(var usuario: String){
+    override fun toString():String {
+        return "$usuario"
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, var duracao: Int){
+    override fun toString():String{
+        return "$nome, Hrs:$duracao"
+    }
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>,val nivel: Nivel) {
+	override fun toString():String{
+        return "Formação: $nome\nConteúdo educacional: $conteudos\nNível: $nivel \nAlunos:$inscritos"
+    }
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuario: Usuario) {
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+       inscritos.addAll(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+ 
+   	val lohan = Usuario("lohan")
+    val joao = Usuario("joao")
+     
+   	val conteudoJava = ConteudoEducacional("java", 90)
+   	val conteudoKotlin = ConteudoEducacional("kotlin", 40)
+     
+    val formacaoJava = Formacao("java Developer", listOf(conteudoJava), Nivel.BASICO)
+    val formacaoKotlin = Formacao("Kotlin Developer", listOf(conteudoKotlin), Nivel.INTERMEDIARIO)
+	
+    formacaoJava.matricular(lohan)
+    formacaoKotlin.matricular(lohan, joao)
+    
+    println(formacaoJava)
+    println()
+   	println(formacaoKotlin)
 }
